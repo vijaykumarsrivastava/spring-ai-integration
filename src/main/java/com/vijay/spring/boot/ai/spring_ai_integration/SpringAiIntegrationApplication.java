39c23c6e1a919f8810b6d13bb3e87b3a0e1c6603
Vijay Kumar <vijaybsacet@yahoo.com>
2026-04-18 23:53:08 +0530

    Init project

---
+++ src/main/java/com/vijay/spring/boot/ai/spring_ai_integration/SpringAiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.spring.boot.ai.spring_ai_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijay/spring/boot/ai/spring_ai_integration/SpringAiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.spring.boot.ai.spring_ai_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
